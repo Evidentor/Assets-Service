0e088071cd1b2d7a42afcb889e278a43a3c8a100
Karlito16 <karlo.dimjasevic@gmail.com>
2025-04-27 23:50:09 +0200

    Initial project setup

---
+++ src/main/java/net/dimjasevic/karlo/fer/evidentor/assets_service/AssetsServiceApplication.java
@@ -0,0 +1,13 @@
+package net.dimjasevic.karlo.fer.evidentor.assets_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dimjasevic/karlo/fer/evidentor/assets_service/AssetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dimjasevic.karlo.fer.evidentor.assets_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
